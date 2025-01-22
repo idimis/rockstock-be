f03fe39e6c0e755fdfd6821862f56f792e59c896
Welk-dy <fernaldysaputra@gmail.com>
2025-01-22 16:44:55 +0700

    First Commit

---
+++ src/main/java/com/purwadhika/rockstock/RockstockApplication.java
@@ -0,0 +1,13 @@
+package com.purwadhika.rockstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RockstockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RockstockApplication.class, args);
+	}
+
+}
